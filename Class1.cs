441d581e6dedc852879b655f60cb69436c8bfb5a
mounika4078 <Mgundu4078@conestogac.on.ca>
2024-06-12 15:27:02 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NUnitTestingLab
+{
+    public class Triangle
+        //Mounika lab03
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+
+        {
+            string result;
+            // revert to correct condition
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+
+    }
+}
